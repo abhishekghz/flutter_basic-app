c8fa98a9fd338f6c7be25d1130fd48d6842a2b22
Abhishek Gautam <75597749+abhishekghz@users.noreply.github.com>
2022-02-24 20:26:04 +0530

    Add files via upload

---
+++ flutter_rectangle/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hello Rectangle',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello Rectangle'),
+        ),
+        body: const HelloRectangle(),
+      ),
+    ),
+  );
+}
+
+class HelloRectangle extends StatelessWidget {
+  const HelloRectangle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.greenAccent,
+        height: 400.0,
+        width: 300.0,
+        child: const Center(
+          child: Text(
+            'Hello Abhishek!',
+            style: TextStyle(fontSize: 40.0),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
